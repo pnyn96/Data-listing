788b6649af270a367fa46b1add67a53b9c1f626c
pnyn96 <106573198+pnyn96@users.noreply.github.com>
2026-01-21 15:54:02 +0530

    Update s3_reader.py

--- app/s3_reader.py
+++ app/s3_reader.py
@@ -4,9 +4,9 @@ import json
 s3 = boto3.client("s3")
 
 def read_json(bucket, key):
-    obj = s3.get_object(Bucket=bucket, Key=key)
-    return json.loads(obj["Body"].read())
+    response = s3.get_object(Bucket=bucket, Key=key)
+    return json.loads(response["Body"].read())
 
 def read_csv(bucket, key):
-    obj = s3.get_object(Bucket=bucket, Key=key)
-    return obj["Body"].read()
+    response = s3.get_object(Bucket=bucket, Key=key)
+    return response["Body"].read()
